a364513a0766ad1feecb73a8a3f52c757f3373e0
tghop <35169460+tghop@users.noreply.github.com>
2018-01-06 10:09:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-
-client.on("message", (message) => {
- 
-  if (message.author.bot) return;
-
-  if (message.content.startsWith("<:dab:399153379412148225>")) {
-    message.channel.send("<:dab:399153379412148225>");
-  } else
-  if (message.content.startsWith("kill")) {
-    message.channel.send("niggers");
-  }
-  
-  if(message.content === "who is the best nigger of them all?") {
-    message.channel.send("AfricanChildWelfare");
-}
-
-if(message.content === "Kill") {
-    message.channel.send("Niggers");
-}
-
-if(message.content === "help") {
-    message.channel.send("Help? HELP?? What sort of bot do you think I am? No one will help you, fool.");
-}
-
-if(message.content === "Help") {
-    message.channel.send("Help? HELP?? What sort of bot do you think I am? No one will help you, fool.");
-}
-
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
