bdcdaeb5d530c2737e5e67b19c41ec7791640333
tghop <35169460+tghop@users.noreply.github.com>
2018-01-06 08:42:39 -0500

    Update discbot.js

--- discbot.js
+++ discbot.js
@@ -32,4 +32,4 @@ if(message.content === "Help") {
 
 });
 
-client.login("Mzk3MjE5OTAxNTU2NjU0MDgw.DSs6dQ.MeBaccWdB7396RxbBK_ACJb8EDU");
+client.login(process.env.Mzk3MjE5OTAxNTU2NjU0MDgw.DSs6dQ.MeBaccWdB7396RxbBK_ACJb8EDU);
